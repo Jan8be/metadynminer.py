37b24cbc1347fe199af218bd84bf357607e80b6d
Jan8be <Jan8be@gmail.com>
2023-04-27 13:45:55 +0200

    bux fix

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ setup(
     author_email='Jan1.Beranek@vscht.cz',
     license='GPL-3.0',
     packages=['metadynminer'],
-    install_requires=['numpy>=1.23.5,<2.0.0',
-                      'matplotlib>=3.7.1,<4.0.0',
-                      'pandas>=1.5.3',
+    install_requires=['numpy',
+                      'matplotlib',
+                      'pandas',
                       'pyvista>=0.38.5'
                       ],
 
